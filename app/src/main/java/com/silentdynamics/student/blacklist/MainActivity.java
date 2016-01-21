c4447f605069b1de754b2e072062529d116e6a14
Robert Richter <Robert Richter>
2016-01-21 15:20:48 +0100

    user registration and conection to rest service added

--- app/src/main/java/com/silentdynamics/student/blacklist/MainActivity.java
+++ app/src/main/java/com/silentdynamics/student/blacklist/MainActivity.java
@@ -8,9 +8,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setAction("Action", null).show();
             }
         });
-        eventsButton = (Button) findViewById(R.id.eventsButton);
+        eventsButton = (Button) findViewById(R.id.findEventsButton);
         eventsButton.setOnClickListener(this);
     }
 
@@ -67,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (networkInfo != null && networkInfo.isConnected()) {
             new DownloadDataTask().execute(url);
         } else {
-            // display error
+            Log.e("Connect", "Couldn't connect");
         }
     }
 }
