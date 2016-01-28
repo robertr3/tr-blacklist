73f6595ddca6067d179d48a46fcc08a96bdbeedd
kuhfisch <kuhfisch@gmail.com>
2016-01-28 21:19:45 +0100

    BatteryReceiver properly handles battery status now. FindEvents focuses map on user location

--- app/src/main/java/com/silentdynamics/student/blacklist/BatteryLevelReceiver.java
+++ app/src/main/java/com/silentdynamics/student/blacklist/BatteryLevelReceiver.java
@@ -13,6 +13,10 @@ import android.widget.Toast;
  */
 public class BatteryLevelReceiver extends BroadcastReceiver {
 
+    boolean batteryLow = false;
+    boolean isConnected = false;
+    boolean batteryOkay = true;
+
     @Override
     public void onReceive(Context context, Intent intent)
     {
@@ -21,23 +25,45 @@ public class BatteryLevelReceiver extends BroadcastReceiver {
         if(intent.getAction().equals(Intent.ACTION_BATTERY_LOW)) {
             Log.d("Battery", "Battery low");
             Toast.makeText(context, "Battery low",Toast.LENGTH_SHORT).show();
+            batteryLow = true;
+            getBatteryStatus();
         }
 
         if(intent.getAction().equals(Intent.ACTION_BATTERY_OKAY)) {
             Log.d("Battery", "Battery okay");
+            batteryLow = false;
+            getBatteryStatus();
         }
 
         if(intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
             Log.d("Battery", "Battery power connected");
             Toast.makeText(context, "Battery power conntected",Toast.LENGTH_SHORT).show();
+            isConnected = true;
+            getBatteryStatus();
         }
 
         if(intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
             Log.d("Battery", "Battery power disconnected");
+            isConnected = false;
+            getBatteryStatus();
         }
 
         if(intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
             Log.d("Battery", "Battery changed");
         }
+    }
+
+    public boolean getBatteryOkay (){
+        return batteryOkay;
+    }
+
+    void getBatteryStatus() {
+        // if battery is okay or connected
+        if(batteryLow == false || isConnected) {
+            batteryOkay = true;
+            return;
+        }
+        // if battery is low and not connected
+        batteryOkay = false;
     }
 }
--- app/src/main/java/com/silentdynamics/student/blacklist/FindEventsActivity.java
+++ app/src/main/java/com/silentdynamics/student/blacklist/FindEventsActivity.java
@@ -1,10 +1,21 @@
 package com.silentdynamics.student.blacklist;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,21 +25,41 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Random;
 
-public class FindEventsActivity extends FragmentActivity implements OnMapReadyCallback, EventFragment.OnFragmentInteractionListener {
+public class FindEventsActivity extends FragmentActivity implements OnMapReadyCallback, EventFragment.OnFragmentInteractionListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private static final String TAG = FindEventsActivity.class.getSimpleName();
 
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient = null;
+    LatLng userPosition = new LatLng(-34, 151);
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private final static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_events);
         Log.d(TAG, "find Events open");
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);*/
-       //mapFragment.getMapAsync(this);
 
+        // Create an instance of GoogleAPIClient
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        //
+        // Fragments
+        //
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
         if (findViewById(R.id.fragment_container) != null) {
@@ -58,10 +89,132 @@ public class FindEventsActivity extends FragmentActivity implements OnMapReadyCa
                 mapFragment.setArguments(getIntent().getExtras());
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.fragment_container, mapFragment).commit();
+                mapFragment.getMapAsync(this);
             }
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    protected void onStart() {
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Location location = null;
+
+        // Check for permission
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Log.d("Permission", "granted");
+
+            location = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            }
+            else {
+                //userPosition = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                handleNewLocation(location);
+            }
+        }
+        else {
+            Log.d("Permission", " not granted");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+        /*//PackageManager pm = this.getPackageManager();
+        //int hasPerm = pm.checkPermission(
+               // Manifest.permission.ACCESS_FINE_LOCATION,
+               // this.getPackageName());
+        if (hasPerm != PackageManager.PERMISSION_GRANTED) {
+            Log.d("Permission", "granted");
+
+        }
+        else {
+            Log.d("Permission", " not granted");
+
+        }*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // Do stuff
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -78,11 +231,16 @@ public class FindEventsActivity extends FragmentActivity implements OnMapReadyCa
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.addMarker(new MarkerOptions().position(userPosition).title("Marker at user position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userPosition));
     }
 
     public void onFragmentInteraction(String id){
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
 
     }
 }
